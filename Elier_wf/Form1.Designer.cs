0e50a4bed7df6e7753776eb7ebf6bd77dde7dd31
Eleinar <danya.moiseeff2015@yandex.ru>
2023-06-05 17:13:19 +0700

    Добавьте файлы проекта.

---
+++ Elier_wf/Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace Elier_wf
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Func_textbox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.x0_textBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.y0_textBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.h_textBox = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Func_textbox
+            // 
+            this.Func_textbox.Location = new System.Drawing.Point(75, 31);
+            this.Func_textbox.Name = "Func_textbox";
+            this.Func_textbox.Size = new System.Drawing.Size(100, 20);
+            this.Func_textbox.TabIndex = 0;
+            this.Func_textbox.TextChanged += new System.EventHandler(this.Func_textbox_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(6, 533);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(243, 60);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Вычислить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Уравнение:";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 272);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(243, 243);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(113, 69);
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(62, 20);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(103, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Количество шагов:";
+            // 
+            // x0_textBox
+            // 
+            this.x0_textBox.Location = new System.Drawing.Point(130, 153);
+            this.x0_textBox.Name = "x0_textBox";
+            this.x0_textBox.Size = new System.Drawing.Size(45, 20);
+            this.x0_textBox.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(18, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "x0";
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 12);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.ScrollGrace = 0D;
+            this.zedGraphControl1.ScrollMaxX = 0D;
+            this.zedGraphControl1.ScrollMaxY = 0D;
+            this.zedGraphControl1.ScrollMaxY2 = 0D;
+            this.zedGraphControl1.ScrollMinX = 0D;
+            this.zedGraphControl1.ScrollMinY = 0D;
+            this.zedGraphControl1.ScrollMinY2 = 0D;
+            this.zedGraphControl1.Size = new System.Drawing.Size(752, 593);
+            this.zedGraphControl1.TabIndex = 9;
+            this.zedGraphControl1.UseExtendedPrintDialog = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.h_textBox);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.y0_textBox);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.numericUpDown1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.Func_textbox);
+            this.groupBox1.Controls.Add(this.x0_textBox);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(770, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(274, 599);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(3, 198);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(18, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "y0";
+            // 
+            // y0_textBox
+            // 
+            this.y0_textBox.Location = new System.Drawing.Point(130, 195);
+            this.y0_textBox.Name = "y0_textBox";
+            this.y0_textBox.Size = new System.Drawing.Size(45, 20);
+            this.y0_textBox.TabIndex = 8;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(77, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Размер шага:";
+            // 
+            // h_textBox
+            // 
+            this.h_textBox.Location = new System.Drawing.Point(130, 109);
+            this.h_textBox.Name = "h_textBox";
+            this.h_textBox.Size = new System.Drawing.Size(45, 20);
+            this.h_textBox.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1025, 623);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Name = "Form1";
+            this.Text = "Метод Эйлера";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Func_textbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox x0_textBox;
+        private System.Windows.Forms.Label label3;
+        private ZedGraph.ZedGraphControl zedGraphControl1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox h_textBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox y0_textBox;
+    }
+}
+
---
+++ Elier_wf/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using org.mariuszgromada.math.mxparser;
+using ZedGraph;
+
+namespace Elier_wf
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.Columns.Add("Xcolumn", "X");
+
+            dataGridView1.Columns.Add("Ycolumn", "Y");
+
+
+        }
+        string Func;
+        private void Func_textbox_TextChanged(object sender, EventArgs e)
+        {
+            Func = Func_textbox.Text;
+        }
+        double x, y;
+        int n;
+        double h;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Func_textbox.Text == "")
+            {
+                MessageBox.Show("Уравнение не может быть пустым", "Ошибка", MessageBoxButtons.OK);
+                return;
+            }
+            try
+            {
+                h = double.Parse(h_textBox.Text);
+                x = double.Parse(x0_textBox.Text);
+                y = double.Parse(y0_textBox.Text);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Некорректное значение h, x0 или y0", "Ошибка", MessageBoxButtons.OK);
+                return;
+            }
+
+            dataGridView1.Rows.Clear();
+            n = (int)numericUpDown1.Value;
+
+           
+            Function F = new Function($"F(x,y) = {Func}");
+
+            double[] X = new double[n + 1];
+            double[] Y = new double[n + 1];
+
+            X[0] = x;
+            Y[0] = y;
+            Argument xn = new Argument("xn", X[0]);
+            Argument yn = new Argument("yn", Y[0]);
+            dataGridView1.Rows.Add(X[0], Y[0]);
+
+            for (int i = 1; i <= n; i++)
+            {
+                X[i] = x + i * h;
+                xn.setArgumentValue(X[i - 1]);
+                yn.setArgumentValue(Y[i - 1]);
+                Expression Exp = new Expression("F(xn,yn)", F, xn, yn);
+
+                Y[i] = Y[i - 1] + h * Exp.calculate();
+
+
+                dataGridView1.Rows.Add(X[i], Y[i]);
+
+                DrawGaphic(X, Y);
+            }
+            
+            
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MessageBox.Show("Выполнил Моисеев Даниил ИС(ПРО)-21");
+        }
+
+        private void DrawGaphic(double[] X, double[] Y)
+        {
+            GraphPane pane = zedGraphControl1.GraphPane;
+            pane.CurveList.Clear();
+
+            PointPairList list = new PointPairList();
+            pane.XAxis.Title.Text = "Ось X";
+            pane.YAxis.Title.Text = "Ось Y";
+            pane.Title.Text = "График";
+
+            for (int i = 0; i <= n; i++)
+            {
+                list.Add(X[i], Y[i]);
+            }
+            LineItem Curva = pane.AddCurve("График", list, Color.Blue, SymbolType.None);
+        }
+    }
+}
